d433999f74b460665bbcb0a40b54f5bed1981464
Timothy Moore <moore.tim@outlook.com>
2017-07-30 14:13:05 -0400

    refactor: Remove showSearchPage state

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import MyReads from './MyReads.js'
 import Search from './Search.js'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage:  false
-  }
 
   render() {
     return (
