190237b97d7fb03672af406cd2603e7d599134e3
Timothy Moore <moore.tim@outlook.com>
2017-07-30 14:05:41 -0400

    refactor: New SearchBooksResults component
    Uses BookGrid to Display list of books in search result

---
+++ src/SearchBooksResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BookGrid from './BookGrid.js';
+
+class SearchBooksResults extends Component {
+  render() {
+    return (
+      <div className="search-books-results">
+        <BookGrid />
+      </div>
+    )
+  }
+}
