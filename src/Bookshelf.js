6d9249f93b53f823d1aeb6153fe6cc2c6d832e00
Timothy Moore <moore.tim@outlook.com>
2017-07-30 14:04:24 -0400

    refactor: Use BookGrid component in Bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import Book from './Book.js'
+import BookGrid from './BookGrid.js'
 
 class Bookshelf extends Component {
   render() {
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">Bookshelf</h2>
-        <ol className="books-grid">
-          <Book />
-          <Book />
-        </ol>
+        <div className="bookshelf-books">
+          <BookGrid />
+        </div>
       </div>
       )
   }
