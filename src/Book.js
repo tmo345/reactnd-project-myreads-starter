a1aca049d93422fb046767c697c41ddeb1d580a8
Timothy Moore <moore.tim@outlook.com>
2017-07-30 16:08:03 -0400

    fix: Add missing div Book component to fix book style

--- src/Book.js
+++ src/Book.js
@@ -5,20 +5,22 @@ class Book extends Component {
     let bookCover = this.props.book.imageLinks.thumbnail;
     return (
       <li>
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookCover})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookCover})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className='book-title'>{this.props.book.title}</div>
+          <div className="book-authors">{this.props.book.authors.join(', ')}</div>
         </div>
-        <div className='book-title'>{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors.join(', ')}</div>
       </li>
       )
   }
