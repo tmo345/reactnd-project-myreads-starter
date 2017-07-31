d66a4511d9f09a5cbbe2e7d56270889960f27ad5
Timothy Moore <moore.tim@outlook.com>
2017-07-31 17:33:46 -0400

    fix: Change to using index as key for list of books rendered
    At times there are books with identical ids returned from the BooksAPI.search call. Tried to use title, but there were sometimes books with same title as well.

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,21 +3,22 @@ import Book from './Book.js';
 
 class Bookshelf extends Component {
   render() {
-    return (
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {this.props.books.map((book) => {
-          return (
-          <Book
-            key={book.id} book={book}
-            shelfChangeHandler={this.props.shelfChangeHandler}
-            shelves={this.props.shelves}
-          />
-          )
-            })}
+      return (
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            { this.props.books.map((book, index) => {
+              return (
+                <Book
+                  key={index} book={book}
+                  shelfChangeHandler={this.props.shelfChangeHandler}
+                  shelves={this.props.shelves}
+                />
+              );
+              })
+            }
           </ol>
         </div>
-        )
+      );
   }
 }
 
