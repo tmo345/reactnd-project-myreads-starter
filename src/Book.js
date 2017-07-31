658ba4acca02d3de24234d567bc903a77b3db31f
Timothy Moore <moore.tim@outlook.com>
2017-07-31 17:31:02 -0400

    fix: Conditionally render book cover and author
    Occasionally, a book without a cover image or author list is returned from calling BooksAPI.search. Conditionally rendering these properties avoids an undefined property error.

--- src/Book.js
+++ src/Book.js
@@ -15,7 +15,9 @@ class Book extends Component {
 
           <div className="book-top">
 
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+            { book.imageLinks !== undefined &&
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+            }
 
             <div className="book-shelf-changer">
               <select value={ book.shelf } onChange={ this.handleShelfSelection }>
@@ -32,7 +34,9 @@ class Book extends Component {
           </div>
 
           <div className='book-title'>{ book.title }</div>
-          <div className="book-authors">{ book.authors.join(', ') }</div>
+          { book.authors !== undefined &&
+            <div className="book-authors">{ book.authors.join(', ') }</div>
+          }
 
         </div>
       </li>
