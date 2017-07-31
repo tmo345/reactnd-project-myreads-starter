9738c9042141e1781bc787ca64cec30701098e83
Timothy Moore <moore.tim@outlook.com>
2017-07-31 17:26:24 -0400

    feat: Add ability to search for book from BooksAPI

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import SearchBooksBar from './SearchBooksBar.js'
 import Bookshelf from './Bookshelf.js';
+import * as BooksAPI from './BooksAPI.js';
 
 class Search extends Component {
   state = {
@@ -8,11 +9,40 @@ class Search extends Component {
     searchResults: []
   }
 
+  updateQuery = (newQuery) => {
+    this.setState({ query: newQuery.trim() })
+  }
+
+  updateSearchResults = (results) => {
+    this.setState({ searchResults: results })
+  }
+
+
+  handleSearch = (newQuery) => {
+    if (newQuery.trim() !== '') {
+      BooksAPI.search(newQuery, 20)
+        .then((books) => {
+          this.updateQuery(newQuery.trim());
+          this.updateSearchResults(books);
+        }, () => {
+          this.updateQuery('');
+          this.updateSearchResults([])
+        })
+    } else {
+      this.updateSearchResults([]);
+    }
+  }
+
+
   render() {
     return (
       <div className="search-books">
-        <SearchBooksBar />
-        <Bookshelf shelves={this.props.shelves} books={this.state.searchResults}/>
+        <SearchBooksBar handleSearch={this.handleSearch} />
+        <div className="search-books-results">
+          { this.state.searchResults.length > 0 &&
+            <Bookshelf shelves={this.props.shelves} books={this.state.searchResults} shelfChangeHandler={this.props.shelfChangeHandler}/>
+          }
+        </div>
       </div>
     )
   }
--- src/SearchBooksBar.js
+++ src/SearchBooksBar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 class SearchBooksBar extends Component {
+  handleQueryChange = (event) => {
+    this.props.handleSearch(event.target.value);
+  }
+
   render() {
     return (
       <div className="search-books-bar">
         <a href="/" className="close-search">Close</a>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" />
+          <input type="text" onInput={this.handleQueryChange} placeholder="Search by title or author" />
         </div>
       </div>
     )
