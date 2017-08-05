34918377b30f102dbc075909a3ab5764ba8b29cc
Timothy Moore <moore.tim@outlook.com>
2017-08-05 10:18:56 -0400

    docs Add more method jsdoc blocks

--- src/Search.js
+++ src/Search.js
@@ -32,8 +32,8 @@ class Search extends Component {
   }
 
   /**
-  * @description Reset state to original emptry string query and empty array searchResults
-  */
+   * @description Reset state to original emptry string query and empty array searchResults
+   */
   resetState = () => {
     this.setState({
       query: '',
@@ -42,10 +42,18 @@ class Search extends Component {
   }
 
   /**
-  * @description Tests for valid query: non-empty string
-  * @param   {string} query
-  * @returns {Bool} True if valid, False if invalid
-  */
+   * @description Clear searchResults state
+   */
+  clearSearchResults = () => {
+    this.setState({
+      searchResults: []
+    })
+  }
+  /**
+   * @description Tests for valid query: non-empty string
+   * @param   {string} query
+   * @returns {Bool} True if valid, False if invalid
+   */
   isValidQuery = (query) => {
     if (typeof query !== 'string' || query.trim().length === 0) {
       return false;
@@ -55,13 +63,13 @@ class Search extends Component {
   }
 
   /**
-  * @description Check results object for presence of 'error' property, which signifies search
-  * returned 0 results
-  * @param  {Book[] | {error: string, items: Array} } results Books.search API returns either an
-  * array of Book objects or an object with an error property if search does not return Book
-  * results.
-  * @returns {Bool} True if Book results returned | False if no Book results returned
-  */
+   * @description Check results object for presence of 'error' property, which signifies search
+   * returned 0 results
+   * @param  {Book[] | {error: string, items: Array} } results Books.search API returns either an
+   * array of Book objects or an object with an error property if search does not return Book
+   * results.
+   * @returns {Bool} True if Book results returned | False if no Book results returned
+   */
   resultsReturned = (results) => {
     if (results.hasOwnProperty('error')) {
       return false;
@@ -70,6 +78,22 @@ class Search extends Component {
     }
   }
 
+  /**
+   * @description Handles searching BooksAPI for books to add to main page bookshelves.
+   *
+   * (For project, only queries in SEARCH_TERMS.md return results.) If query validates,
+   * query state is updated and sent with ajax call BooksAPI.search. A promise is
+   * returned.
+   *   On fulfillment:
+   *    - If results return, the bookshelf state is synced between search results and
+   *      the user's bookshelves. The searchResults state is then updated with synced
+   *      results.
+   *    - If no results return, clear search Results.
+   *   On rejection:
+   *    - Reset query and searchResults state
+   *
+   * @param   {string} newQuery
+   */
   handleSearch = (newQuery) => {
     if (! this.isValidQuery(newQuery)) {
       this.resetState();
@@ -85,7 +109,7 @@ class Search extends Component {
           let syncedResults = this.syncResultsWithMyBooks(results);
           this.updateSearchResults(syncedResults);
         } else {
-          this.resetState();
+          this.clearSearchResults();
         }
       }, (err) => {
         this.resetState();
@@ -93,6 +117,9 @@ class Search extends Component {
       });
   }
 
+  /**
+   * @description
+   */
   syncResultsWithMyBooks = (results) => {
     let syncedResults = results.map((result) => {
       let matchingBook = this.props.myBooks.find((myBook) => {
@@ -106,6 +133,8 @@ class Search extends Component {
     });
     return syncedResults;
   }
+
+
 
   changeSearchResultShelf = (resultBookToChange, shelf) => {
     this.setState({
@@ -122,23 +151,23 @@ class Search extends Component {
   render() {
     return (
       <div className="search-books">
-        <SearchBooksBar
-          handleSearch={this.handleSearch}
-          handleCloseSearch={this.props.handleCloseSearch}
+      <SearchBooksBar
+      handleSearch={this.handleSearch}
+      handleCloseSearch={this.props.handleCloseSearch}
+      />
+      <div className="search-books-results">
+      { this.state.searchResults.length > 0 &&
+        <Bookshelf
+        books={this.props.books}
+        shelves={this.props.shelves}
+        searchResults={this.state.searchResults}
+        shelfChangeHandler={this.props.shelfChangeHandler}
+        changeSearchResultShelf={this.changeSearchResultShelf}
         />
-        <div className="search-books-results">
-          { this.state.searchResults.length > 0 &&
-          <Bookshelf
-            books={this.props.books}
-            shelves={this.props.shelves}
-            searchResults={this.state.searchResults}
-            shelfChangeHandler={this.props.shelfChangeHandler}
-            changeSearchResultShelf={this.changeSearchResultShelf}
-          />
 
-        }
+      }
       </div>
-    </div>
+      </div>
     )
   }
 }
